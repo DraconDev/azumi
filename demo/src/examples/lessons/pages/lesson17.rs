c7d433cf727a8ddae0e394134844b2794caed98c
DraconDev <dracsharp@gmail.com>
2025-12-09 09:01:17 +0000

    feat(lesson17): Use variables for class and style attributes

--- demo/src/examples/lessons/pages/lesson17.rs
+++ demo/src/examples/lessons/pages/lesson17.rs
@@ -3,13 +3,16 @@ use azumi::prelude::*;
 
 #[azumi::component]
 fn Lesson17() -> Html {
+    let container_class = "lesson-container";
+    let img_style = "border: 2px solid red; width: 100px;";
+
     html! {
-        <div class="lesson-container">
+        <div class={container_class}>
             <h1>"Lesson 17: Asset Pipeline"</h1>
             <p>"This image path should be rewritten to include a hash:"</p>
 
             // This should be rewritten to /assets/test_logo.<hash>.png
-            <img src="/static/test_logo.png" alt="Test Logo" style="border: 2px solid red; width: 100px;" />
+            <img src="/static/test_logo.png" alt="Test Logo" style={img_style} />
 
             <p>"If you inspect the element, the src should start with /assets/..."</p>
         </div>
