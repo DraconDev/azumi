a8f21b1531251237b52c5f11153bd4c1d7fc3415
DraconDev <dracsharp@gmail.com>
2025-12-11 17:28:48 +0000

    test(demo): reorder elements in HTML test

--- demo/tests/pass_correct_order.rs
+++ demo/tests/pass_correct_order.rs
@@ -2,16 +2,17 @@ use azumi::html;
 
 fn main() {
     html! {
-        <script>
-            // Script at top is OK
-            console.log("ok");
-        </script>
-
-        <div>"Content in middle is OK"</div>
+        // <script>
+        //     // Script at top is OK
+        //     console.log("ok");
+        // </script>
 
         <style>
             /* Style at bottom is OK */
             .foo { color: green; }
         </style>
+
+        <div>"Content in middle is OK"</div>
+
     };
 }
