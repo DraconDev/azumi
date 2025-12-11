3d7db41f4ef9dee0575aa4550ca7c43aa1461559
DraconDev <dracsharp@gmail.com>
2025-12-11 17:51:59 +0000

    refactor(examples): remove inline css styles

--- demo/src/examples/css_variables.rs
+++ demo/src/examples/css_variables.rs
@@ -4,21 +4,7 @@ use azumi::html;
 pub fn css_variables_demo() -> impl azumi::Component {
     let percentage = "50%";
     html! {
-        <style>
-            .progress_bar {
-                background-color: "#e0e0e0";
-                border-radius: "4px";
-                height: "20px";
-                width: "100%";
-            }
-            .progress_value {
-                background-color: "#76c7c0";
-                border-radius: "4px";
-                height: "100%";
-                width: "var(--width)";
-                transition: "width 0.5s ease-in-out";
-            }
-        </style>
+
         <div class={progress_bar} --width={percentage}>
             <div class={progress_value}></div>
         </div>
