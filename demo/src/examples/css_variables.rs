613c82822dd6b0ad3d2978e9e79e4e903f79092e
DraconDev <dracsharp@gmail.com>
2025-12-11 17:24:52 +0000

    style(demo): add inline CSS for progress bar

--- demo/src/examples/css_variables.rs
+++ demo/src/examples/css_variables.rs
@@ -3,8 +3,6 @@ use azumi::html;
 #[azumi::component]
 pub fn css_variables_demo() -> impl azumi::Component {
     let percentage = "50%";
-    let _color = "yellow";
-
     html! {
         <div class={progress_bar} --width={percentage}>
             <div class={progress_value}></div>
@@ -12,7 +10,21 @@ pub fn css_variables_demo() -> impl azumi::Component {
         <div --static-var="100px">
             "Static Var"
         </div>
-
+        <style>
+            .progress_bar {
+                background-color: "#e0e0e0";
+                border-radius: "4px";
+                height: "20px";
+                width: "100%";
+            }
+            .progress_value {
+                background-color: "#76c7c0";
+                border-radius: "4px";
+                height: "100%";
+                width: "var(--width)";
+                transition: "width 0.5s ease-in-out";
+            }
+        </style>
     }
 }
 
