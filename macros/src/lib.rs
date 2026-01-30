c458b2316b67f7104435b7088a6b9a1c009b0e58
DraconDev <dracsharp@gmail.com>
2026-01-30 13:11:00 +0000

    brain: auto-sync | SCOPE: macros/src/lib.rs | SYMBOLS: macros/src/lib.rs | LOGIC: structural synchronization | VERIFY: UNCHECKED | FILES: 1

--- macros/src/lib.rs
+++ macros/src/lib.rs
@@ -799,20 +799,20 @@ fn validate_nodes(
                             let value_str = let_block.value.to_string().trim_matches('"').to_string();
                             
                             let msg = format!(
-                                "ANTI-PATTERN: @let {} = \"{}\"\n\n\
+                                "ANTI-PATTERN: @let {0} = \"{1}\"\n\n\
                                 Using @let to define CSS class names is NOT allowed in Azumi.\n\
                                 CSS classes must be defined in <style> blocks, not as variables.\n\n\
                                 CORRECT - Define in <style> block:\n\
-                                    <div class={{{{var}}}>...</div>\n\
+                                    <div class={{{{0}}}>...</div>\n\
                                     <style>\n\
-                                        .{}{{ ... }}\n\
+                                        .{0}{{ ... }}\n\
                                     </style>\n\n\
                                 INCORRECT - Using @let for classes:\n\
-                                    @let {var} = \"{}\";  // DON'T DO THIS!\n\
-                                    <div class={{{{var}}}>...</div>\n\n\
+                                    @let {0} = \"{1}\";  // DON'T DO THIS!\n\
+                                    <div class={{{{0}}}>...</div>\n\n\
                                 The <style> block automatically creates the variable for you.\n\
                                 See: AI_GUIDE_FOR_WRITING_AZUMI.md - Critical Rules section",
-                                var_name, value_str, var_name, value_str
+                                var_name, value_str
                             );
                             errors.push(quote_spanned! { let_block.span =>
                                 compile_error!(#msg);
