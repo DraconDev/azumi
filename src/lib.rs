c0c9a0f1f03c6f8be309c4cfdbcb36c22204c86c
DraconDev <dracsharp@gmail.com>
2026-04-13 16:26:43 +0100

    chore(misc): Security hardening of script injection in HTML output
    # Project State
    
    ## Current Focus
    Security hardening of script injection in HTML output
    
    ## Completed
    - [x] Added escaping for `</script>` tags in generated HTML script tags to prevent injection vulnerabilities
    ---
    category: chore
    scope: misc
    ---

--- src/lib.rs
+++ src/lib.rs
@@ -536,7 +536,7 @@ pub const AZUMI_JS: &str = include_str!("client.min.js");
 /// Helper to generate the <script> tag for the client runtime
 /// Usage: html! { <head> { azumi::azumi_script() } ... </head> }
 pub fn azumi_script() -> String {
-    format!(r#"<script>{}</script>"#, AZUMI_JS)
+    format!(r#"<script>{}</script>"#, AZUMI_JS.replace("</script>", r"<\/script>"))
 }
 
 pub struct HotReloadClosure<'a>(pub &'a dyn Fn(&mut std::fmt::Formatter<'_>) -> std::fmt::Result);
